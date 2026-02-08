3eb02bb586ea90f319fc4e657a1920c14366887c
Thomas Gebert <thomas.gebert78@gmail.com>
2026-02-08 15:19:59 +0100

    Fix Usage info

--- freebox-cli.sh
+++ freebox-cli.sh
@@ -34,7 +34,11 @@ cat << HELP
 
 Freebox API CLI
 
+<<<<<<< HEAD
 Usage: $1 [OPTION]
+=======
+Usage: $(basename $0) [OPTIONS]
+>>>>>>> 887060c (Initial release)
 
 Mandatory options:
 A command to execute
